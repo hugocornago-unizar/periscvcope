617f1f99b26a48b3a6ea8406416554e8cc59bf39
Hugo <hugo@zuperito.xyz>
2026-02-23 18:21:44 +0100

    refine opcodes

--- src/instruction/parser.rs
+++ src/instruction/parser.rs
@@ -13,6 +13,7 @@ macro_rules! define_instructions {
         $(,)?
     ) => {
         #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        #[allow(non_camel_case_types)]
         pub enum Op {
             $($variant),*
         }
@@ -24,6 +25,7 @@ macro_rules! define_instructions {
                 }
             }
         }
+
 
         pub fn decode_op(opcode: u8, funct3: u8, funct7: u8) -> Option<Op> {
             match (opcode, funct3, funct7) {
@@ -43,50 +45,57 @@ macro_rules! define_instructions {
     (@field) => { _ };
 }
 
+
+impl std::fmt::Display for Op {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 define_instructions! {
-    Add  : R { opcode: 0b0110011, funct3: 0x0, funct7: 0x00 },
-    Sub  : R { opcode: 0b0110011, funct3: 0x0, funct7: 0x20 },
-    Sll  : R { opcode: 0b0110011, funct3: 0x1, funct7: 0x00 },
-    Slt  : R { opcode: 0b0110011, funct3: 0x2, funct7: 0x00 },
-    Sltu : R { opcode: 0b0110011, funct3: 0x3, funct7: 0x00 },
-    Xor  : R { opcode: 0b0110011, funct3: 0x4, funct7: 0x00 },
-    Srl  : R { opcode: 0b0110011, funct3: 0x5, funct7: 0x00 },
-    Sra  : R { opcode: 0b0110011, funct3: 0x5, funct7: 0x20 },
-    Or   : R { opcode: 0b0110011, funct3: 0x6, funct7: 0x00 },
-    And  : R { opcode: 0b0110011, funct3: 0x7, funct7: 0x00 },
+    add  : R { opcode: 0b0110011, funct3: 0x0, funct7: 0x00 },
+    sub  : R { opcode: 0b0110011, funct3: 0x0, funct7: 0x20 },
+    sll  : R { opcode: 0b0110011, funct3: 0x1, funct7: 0x00 },
+    slt  : R { opcode: 0b0110011, funct3: 0x2, funct7: 0x00 },
+    sltu : R { opcode: 0b0110011, funct3: 0x3, funct7: 0x00 },
+    xor  : R { opcode: 0b0110011, funct3: 0x4, funct7: 0x00 },
+    srl  : R { opcode: 0b0110011, funct3: 0x5, funct7: 0x00 },
+    sra  : R { opcode: 0b0110011, funct3: 0x5, funct7: 0x20 },
+    or   : R { opcode: 0b0110011, funct3: 0x6, funct7: 0x00 },
+    and  : R { opcode: 0b0110011, funct3: 0x7, funct7: 0x00 },
 
-    Slli  : I { opcode: 0b0010011, funct3: 0x1, funct7: 0x00 },
-    Srli  : I { opcode: 0b0010011, funct3: 0x5, funct7: 0x00 },
-    Srai  : I { opcode: 0b0010011, funct3: 0x5, funct7: 0x20 },
+    slli  : I { opcode: 0b0010011, funct3: 0x1, funct7: 0x00 },
+    srli  : I { opcode: 0b0010011, funct3: 0x5, funct7: 0x00 },
+    srai  : I { opcode: 0b0010011, funct3: 0x5, funct7: 0x20 },
 
-    Addi  : I { opcode: 0b0010011, funct3: 0x0 },
-    Slti  : I { opcode: 0b0010011, funct3: 0x2 },
-    Sltiu : I { opcode: 0b0010011, funct3: 0x3 },
-    Xori  : I { opcode: 0b0010011, funct3: 0x4 },
-    Ori   : I { opcode: 0b0010011, funct3: 0x6 },
-    Andi  : I { opcode: 0b0010011, funct3: 0x7 },
+    addi  : I { opcode: 0b0010011, funct3: 0x0 },
+    slti  : I { opcode: 0b0010011, funct3: 0x2 },
+    sltiu : I { opcode: 0b0010011, funct3: 0x3 },
+    xori  : I { opcode: 0b0010011, funct3: 0x4 },
+    ori   : I { opcode: 0b0010011, funct3: 0x6 },
+    andi  : I { opcode: 0b0010011, funct3: 0x7 },
 
-    Lb  : I { opcode: 0b0000011, funct3: 0x0 },
-    Lh  : I { opcode: 0b0000011, funct3: 0x1 },
-    Lw  : I { opcode: 0b0000011, funct3: 0x2 },
-    Lbu : I { opcode: 0b0000011, funct3: 0x4 },
-    Lhu : I { opcode: 0b0000011, funct3: 0x5 },
+    lb  : I { opcode: 0b0000011, funct3: 0x0 },
+    lh  : I { opcode: 0b0000011, funct3: 0x1 },
+    lw  : I { opcode: 0b0000011, funct3: 0x2 },
+    lbu : I { opcode: 0b0000011, funct3: 0x4 },
+    lhu : I { opcode: 0b0000011, funct3: 0x5 },
 
-    Sb : S { opcode: 0b0100011, funct3: 0x0 },
-    Sh : S { opcode: 0b0100011, funct3: 0x1 },
-    Sw : S { opcode: 0b0100011, funct3: 0x2 },
+    sb : S { opcode: 0b0100011, funct3: 0x0 },
+    sh : S { opcode: 0b0100011, funct3: 0x1 },
+    sw : S { opcode: 0b0100011, funct3: 0x2 },
 
-    Beq  : B { opcode: 0b1100011, funct3: 0x0 },
-    Bne  : B { opcode: 0b1100011, funct3: 0x1 },
-    Blt  : B { opcode: 0b1100011, funct3: 0x4 },
-    Bge  : B { opcode: 0b1100011, funct3: 0x5 },
-    Bltu : B { opcode: 0b1100011, funct3: 0x6 },
-    Bgeu : B { opcode: 0b1100011, funct3: 0x7 },
+    beq  : B { opcode: 0b1100011, funct3: 0x0 },
+    bne  : B { opcode: 0b1100011, funct3: 0x1 },
+    blt  : B { opcode: 0b1100011, funct3: 0x4 },
+    bge  : B { opcode: 0b1100011, funct3: 0x5 },
+    bltu : B { opcode: 0b1100011, funct3: 0x6 },
+    bgeu : B { opcode: 0b1100011, funct3: 0x7 },
 
-    Lui   : U { opcode: 0b0110111 },
-    Auipc : U { opcode: 0b0010111 },
+    lui   : U { opcode: 0b0110111 },
+    auipc : U { opcode: 0b0010111 },
 
-    Jal  : J { opcode: 0b1101111 },
+    jal  : J { opcode: 0b1101111 },
 
-    Jalr : I { opcode: 0b1100111, funct3: 0x0 },
+    jalr : I { opcode: 0b1100111, funct3: 0x0 },
 }
